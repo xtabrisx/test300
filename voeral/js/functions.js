9548eb60648992cce2813126fa6928328be1d549
xtabrisx <alberto.tocados@gmail.com>
2017-04-22 14:00:10 +0200

    scaffolding main screen

--- voeral/js/functions.js
+++ voeral/js/functions.js
@@ -1,5 +1,9 @@
 $(document).ready(function($) {
 		
+	var winHeight = $(window).height();
+	console.log(winHeight);
+	$(".main-screen").height(winHeight);
+	$(".col-container").height(winHeight);
 	$("img").unveil();
 	//alert("test");
 
